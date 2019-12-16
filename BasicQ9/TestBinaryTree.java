e5d75568d90aa7a8603ad743f1879744ac88e3bf
chcleung1016 <31538842+chcleung1016@users.noreply.github.com>
2019-12-16 20:00:10 +0800

    Create TestBinaryTree.java

---
+++ BasicQ9/TestBinaryTree.java
@@ -0,0 +1,30 @@
+
+package BasicQ9;
+import java.util.*;
+
+//Test program for BasicQ9
+public class TestBinaryTree {
+	
+	public static void main(String[] args) {
+		
+		int data = 0;
+		BinarySearchTree tree = new BinarySearchTree();
+		
+		//Make a scanner using Scanner class to let user input a number
+		Scanner input = new Scanner(System.in);
+		
+		for (int k = 1; k <= 10; k++) {
+			//Get the user's input
+			System.out.print("Enter a number: ");
+			int inputnum = input.nextInt();
+			System.out.print(inputnum + " ");
+			tree.insert(inputnum);
+			System.out.println();
+		}
+		System.out.println();
+		
+		tree.inorder();
+
+	}
+
+}
